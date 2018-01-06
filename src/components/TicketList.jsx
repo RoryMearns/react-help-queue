884e23673393bc22ae5eda762e20e393d9ed5a3b
Rory Mearns <mearns@protonmail.com>
2018-01-06 13:54:10 +1300

    Props

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -3,9 +3,12 @@ import Ticket from "./Ticket";
 
 function TicketList(props){
 
-  return (
-    <Ticket />
-  );
+return (
+    <Ticket 
+        location="3a" 
+        names="Thato and Haley" 
+        issue="Firebase won't save record"/>
+    );
 }
 
 export default TicketList;
